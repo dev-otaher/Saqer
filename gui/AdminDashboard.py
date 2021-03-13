0d65c651aa00b335eb421d08e6f7b05f777096eb
dev-otaher <dev.omar.taher@gmail.com>
2021-03-13 16:42:02 +0300

    Code refactor.
    1. Deleted application folder.
    2. Created "goto" function.
    3. Set path field as disabled.

--- gui/AdminDashboard.py
+++ gui/AdminDashboard.py
@@ -1,5 +1,7 @@
+from functools import partial
+
 from PyQt5.QtWidgets import QDialog
-from PyQt5 import uic,QtCore, QtGui
+from PyQt5 import uic, QtCore, QtGui
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QFileDialog
 from PyQt5.QtCore import Qt, QVariant
@@ -10,29 +12,28 @@ from gui import Login, Warning
 from PyQt5.QtCore import Qt, QTimer
 
 
-
-#each interface defined in a class
+# each interface defined in a class
 class AdminDashboard(QDialog):
-    #cnstructor of the class
     def __init__(self):
         super(AdminDashboard, self).__init__()
         uic.loadUi("gui/interfaces/AdminDashboard.ui", self)
         self.setWindowFlags(QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint))
         self.i_header.mouseMoveEvent = self.move_window
+
         self.i_close.clicked.connect(lambda: exit())
         self.i_minmize.clicked.connect(lambda: self.showMinimized())
-        self.i_register_student.clicked.connect(self.register_student)
-        self.i_train_model.clicked.connect(self.train_model)
-        self.i_offline_atten.clicked.connect(self.offline_attendance)
-        self.i_settings.clicked.connect(self.settings)
+        self.i_logout.mousePressEvent = self.logout
+
+        self.i_register_student.clicked.connect(partial(self.goto, self.i_register_sec))
+        self.i_train_model.clicked.connect(partial(self.goto, self.i_train_sec))
+        self.i_offline_atten.clicked.connect(partial(self.goto, self.i_offline_sec))
+        self.i_settings.clicked.connect(partial(self.goto, self.i_settings_sec))
+
         self.i_video_note.setHidden(True)
         self.i_progress_label.setHidden(True)
         self.i_progress_bar.setHidden(True)
-        self.i_choose_video.clicked.connect(self.choose_video)
-        self.i_start.clicked.connect(self.start_offline_attendance)
-        self.i_logout.mousePressEvent = self.logout
-        self.show()
 
+        self.show()
 
     def move_window(self, e):
         if e.buttons() == Qt.LeftButton:
@@ -40,42 +41,30 @@ class AdminDashboard(QDialog):
             self.clickPosition = e.globalPos()
             e.accept()
 
-    def mousePressEvent(self, event):
-        self.clickPosition = event.globalPos()
+    def mousePressEvent(self, e):
+        self.clickPosition = e.globalPos()
 
-
-    def register_student(self):
-        self.i_choices.setCurrentWidget(self.i_register_sec)
-
-    def train_model(self):
-        self.i_choices.setCurrentWidget(self.i_train_sec)
-
-    def offline_attendance(self):
-        self.i_choices.setCurrentWidget(self.i_offline_sec)
-
-
-    def settings(self):
-        self.i_choices.setCurrentWidget(self.i_settings_sec)
+    def goto(self, widget):
+        self.i_choices.setCurrentWidget(widget)
+        if widget is self.i_offline_sec:
+            self.i_choose_video.clicked.connect(self.choose_video)
+            self.i_start.clicked.connect(self.start_offline_attendance)
 
     def choose_video(self):
-        video_path = QFileDialog.getOpenFileName(self,'Choose Video', '', 'Video (*.mp4 , *.mkv)')
+        video_path = QFileDialog.getOpenFileName(self, 'Choose Video...', '', 'Video (*.mp4 , *.mkv , *.MOV)')
         self.i_video_path.setText(video_path[0])
 
     def start_offline_attendance(self):
-        video_path = self.i_video_path.text()
-        find_mp4 = video_path.find('.mp4')
-        find_mkv = video_path.find('.mkv')
-
-        if find_mp4 == -1 and find_mkv == -1 :
+        self.i_video_note.setHidden(True)
+        if self.i_video_path.text() == "":
             self.i_video_note.setHidden(False)
         else:
-            self.i_video_note.setHidden(True)
             self.i_progress_label.setHidden(False)
             self.i_progress_bar.setHidden(False)
+            pass
 
-    def logout(self, eve):
+    def logout(self):
         try:
-
             Login.Login()
             self.destroy()
         except Exception as e:
