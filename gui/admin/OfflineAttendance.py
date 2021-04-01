ae3c8a507df2233c745ac5f417cc5bda9b17f134
dev-otaher <dev.omar.taher@gmail.com>
2021-04-01 23:37:35 +0300

    Added total checkpoints counter to "No. of Appear" column

--- gui/admin/OfflineAttendance.py
+++ gui/admin/OfflineAttendance.py
@@ -86,14 +86,15 @@ class OfflineAttendance:
 
     def fill_recheck_table(self):
         self.show_recheck_table()
+        checkpoints = self.parent.i_progress_bar.maximum()
         for std in self.students:
             self.parent.i_recheck_table.insertRow(0)
             self.parent.i_recheck_table.setItem(0, 0, QtWidgets.QTableWidgetItem(std.uni_id))
             self.parent.i_recheck_table.setItem(0, 1, QtWidgets.QTableWidgetItem(std.name))
-            self.parent.i_recheck_table.setItem(0, 2, QtWidgets.QTableWidgetItem(str(std.appear_counter)))
+            self.parent.i_recheck_table.setItem(0, 2, QtWidgets.QTableWidgetItem(str(std.appear_counter)+f"/{checkpoints}"))
             checkbox = QtWidgets.QCheckBox()
-            check_state: bool = std.appear_counter/self.parent.i_progress_bar.maximum() > 0.7
-            checkbox.setChecked(check_state)
+            is_present = std.appear_counter/self.parent.i_progress_bar.maximum() > 0.7
+            checkbox.setChecked(is_present)
             self.parent.i_recheck_table.setCellWidget(0, 3, checkbox)
 
     def save_data(self):
